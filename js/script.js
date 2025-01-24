ca9f950d7e902ccf40cdc04819b0be99b06da11e
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-24 20:15:21 +0100

    [creation file]

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// prova
+console.log("funziona");
+
+// variabile output
+const container = document.getElementById('container');
+
+// facciamo la chiamata ajax ad api
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+    .then(response => {
+        console.log(response);
+
+        const arrayList = response.data;
+        console.log(arrayList);
+
+        
+        // quindi estrapoliamo gli oggetti singoli dell'array con un ciclo
+        for(i =0; i < arrayList.length; i++){
+            console.log(arrayList[i]);
+        }
+
+
+
+});
